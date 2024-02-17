a6a738bca91f75a9e5e5eece6f4ad26ba2b1fefb
GGTuran <tayyabturan67@gmail.com>
2024-02-17 12:02:09 +0600

    llogin button

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
 
 
 const Header = () => {
     return (
         <div>
-            <h2>It is Header bro!!!</h2>
+            <Link to="/">Home</Link>
+            <Link to="/login">Login</Link>
         </div>
     );
 };
