b18c7c04f7790c569283ea57acbc4f9e93789846
GGTuran <tayyabturan67@gmail.com>
2024-02-17 12:29:13 +0600

    sign in wiht pop up adding

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,9 +1,20 @@
-
+/* eslint-disable no-unused-vars */
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import app from "../../Firebase/firebase.init";
 
 const Login = () => {
+    const auth = getAuth(app);
+    const provider = new GoogleAuthProvider();
+
+    const handleGoogleSignIn = () =>{
+        signInWithPopup(auth, provider)
+        .then()
+        .catch()
+
+    }
     return (
         <div>
-            <button>Google login</button>
+            <button onClick={handleGoogleSignIn}>Google login</button>
         </div>
     );
 };
