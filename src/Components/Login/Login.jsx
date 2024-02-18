42b448245ff5f38672f9e7d7fd896f054c428dd8
GGTuran <tayyabturan67@gmail.com>
2024-02-18 12:02:54 +0600

    trying to display users

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,16 +1,19 @@
 /* eslint-disable no-unused-vars */
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import app from "../../Firebase/firebase.init";
+import { useState } from "react";
 
 const Login = () => {
+    const [user, setUser] = useState;
     const auth = getAuth(app);
     const provider = new GoogleAuthProvider();
 
     const handleGoogleSignIn = () =>{
         signInWithPopup(auth, provider)
         .then(result => {
-            const user = result.user;
-            console.log(user)
+            const loggedInUser = result.user;
+            console.log(loggedInUser);
+            setUser(loggedInUser);
 
         })
         .catch(error =>{
